c7929607d10771696e602cae7843515ba7414c75
matdavy <davy.mathew@gmail.com>
2025-09-07 17:41:49 +1200

    Adding wall boundaries

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     private Animator animator;
-    [SerializeField] float speed = 2f;
-    Vector2 motionVector;
+    [SerializeField] float speed = 4f;
+    private Vector2 motionVector;
 
 
     void Start()
@@ -26,8 +26,17 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", motionVector.x);
+            animator.SetFloat("LastInputY", motionVector.y);
+
+        }
+        motionVector = context.ReadValue<Vector2>();
         animator.SetFloat("InputX", motionVector.x);
         animator.SetFloat("InputY", motionVector.y);
-        motionVector = context.ReadValue<Vector2>();
+
     }
 }
